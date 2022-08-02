2e2eed0bdc89f0d5504ddd7e9b99a942210ffcfa
petushka1 <nadya@devery.io>
2022-08-02 03:28:32 +0300

    work on fetch

--- src/redux/Missions.js
+++ src/redux/Missions.js
@@ -1,11 +1,9 @@
 const URL = 'https://api.spacexdata.com/v3/missions';
 const fetch = 'MISSIONS_FETCHED';
 
-export const missionsDisplayed = ({ mission_id, mission_name, description }) => ({
+export const missionsDisplayed = (missions) => ({
     type: fetch,
-    mission_id,
-    mission_name, 
-    description
+   missions,
 })
 
 export const missionReducer = (state = [], action) => {
@@ -21,8 +19,7 @@ export const fetchMissions = () => async (dispatch) => {
     await fetch(URL)
     .then((res) => res.json())
     .then((data) => {
-        const MissionList = data
-        dispatch(missionsDisplayed(MissionList))
+        dispatch(missionsDisplayed(data))
     })
 }
 
