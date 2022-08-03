a4defb91e055f585d0b1c980056781edce770006
Karthik suryadevara <karthiksurya2022@gmail.com>
2022-08-04 01:49:49 +0530

    remove all data being stored in redux-store

--- src/components/rocketListing/RocketListing.js
+++ src/components/rocketListing/RocketListing.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { getAllRockets } from '../../redux/rocketSlice'
-import './RocketListing.scss'
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllRockets } from "../../redux/rocketSlice";
+import "./RocketListing.scss";
 
-function RocketListing() {
+function RocketListing({ rockets }) {
+  // const rockets = useSelector(getAllRockets)
+  // console.log(rockets)
 
-  const rockets = useSelector(getAllRockets)
-  console.log(rockets)
+  const displayRockets =
+    rockets != null ? (
+      rockets.map((rocket) => {
+        return (
+          <div className="rocket_listing" key={rocket.id}>
+            <div>
+              <img
+                src={rocket.flickr_images[0]}
+                alt="Rocket 1"
+                className="rocket_image"
+              />
+            </div>
 
-  const displayRockets = rockets.map((rocket) => {
-    return (
-      <div className='rocket_listing' key={rocket.id}>
-        <div>
-            <img src={rocket.flickr_images[0]} alt="Rocket 1" className='rocket_image'/>
-        </div>
+            <div className="rocket_details">
+              <h2>{rocket.rocket_name}</h2>
+              <p>{rocket.description}</p>
+              <button className="reserve_rocket">Reserve Rocket</button>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <p>loading...</p>
+    );
 
-        <div className='rocket_details'>
-          <h2>{rocket.rocket_name}</h2>
-          <p>{rocket.description}</p>
-          <button className='reserve_rocket'>Reserve Rocket</button>
-        </div>
-      </div>
-    )
-  })
-
-
-  return (
-    <div>{displayRockets}</div>
-  )
+  return <div>{displayRockets}</div>;
 }
 
-export default RocketListing
+export default RocketListing;
--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,19 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchAsyncRockets } from '../../redux/rocketSlice';
 import RocketListing from '../rocketListing/RocketListing';
+import axios from "axios";
+
 
 function Rockets() {
 
   const dispatch = useDispatch()
+  const [rockets, setRockets] = useState(null)
 
   useEffect(() => {
-    dispatch(fetchAsyncRockets())
+    async function fetchData() {
+      const rocketData = await axios.get(
+        "https://api.spacexdata.com/v3/rockets"
+      );
+      console.log(rocketData.data)
+      setRockets(rocketData.data);
+    }
+    fetchData()
+    // dispatch(fetchAsyncRockets())
   }, [dispatch])
 
   return (
     <div>
-      <RocketListing />
+      <RocketListing rockets={rockets} />
     </div>
   )
 }
